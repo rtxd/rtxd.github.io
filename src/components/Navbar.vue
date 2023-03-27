e442f7de9be767cd499be71194939e15e734b83c
Alejandro Munoz <alex@digitalmaas.com>
2023-03-27 16:19:56 +1100

    fix vuetify components not displaying

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-app-bar :elevation="2" color="primary" density="comfortable">Alejandro's Website</v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import HomePage from './components/HomePage.vue'
-import AboutPage from './components/AboutPage.vue'
+import HomePage from './components/pages/Home.vue'
+import AboutPage from './components/pages/About.vue'
+import { VApp, VMain, VContainer, VAppBar } from 'vuetify/components'
 loadFonts()
 
 const routes = [
@@ -17,7 +18,11 @@ const router = createRouter({
     routes,
 })
 
-createApp(App)
-  .use(router)
-  .use(vuetify)
-  .mount('#app')
+const app = createApp(App).use(router).use(vuetify)
+
+app.component('v-app', VApp)
+app.component('v-main', VMain)
+app.component('v-container', VContainer)
+app.component('v-app-bar', VAppBar)
+
+app.mount('#app')
