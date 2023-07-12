69b9114399aba57a4d1b75a328eaaa3bb28bf060
Alejandro Munoz <alex@digitalmaas.com>
2023-07-12 19:11:25 +1000

    fix linting and prettier, installed veutify module

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-text-field />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/eslint-module',
+        [
+            '@nuxtjs/eslint-module',
+            {
+                /* module options */
+            },
+        ],
+        '@nuxtjs/tailwindcss',
+        '@nuxt/content',
+        '@invictus.codes/nuxt-vuetify',
+    ],
+    vuetify: {
+        moduleOptions: {
+            /* nuxt-vuetify module options */
+            treeshaking: true,
+            useIconCDN: true,
+
+            /* vite-plugin-vuetify options */
+            styles: true,
+            autoImport: true,
+            useVuetifyLabs: true,
+        },
+    },
 })
